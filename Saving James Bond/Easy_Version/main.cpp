8bc28f42e3bc3e13239ce1c1a542d61c5110dacb
block41 <xy.zou@qq.com>
2021-05-27 11:51:43 +0800

    update

---
+++ Saving James Bond/Easy_Version/main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <queue>
+#include <math.h>
+
+#define MaxVertexNum 101    //最大顶点数设为101，最大鳄鱼数加初始位置
+
+using namespace std;
+using Vertex = int;         // 用顶点下标表示顶点,为整型
+using WeightType = int;   //定义权重类型为int
+
+using point = struct Node;
+struct Node {    //坐标
+    int x;
+    int y;
+};
+
+//图
+class Graph {
+public:
+    int Nv;  // 顶点数
+    int Ne;  // 边数 
+    int jump;  //跳跃距离
+    WeightType G[MaxVertexNum][MaxVertexNum]; // 邻接矩阵 
+    point* Data;   //存放点信息
+    bool* Visited;  //标志是否访问过
+public:
+    Graph(int VertexNum, int jump_);  //带参构造函数
+    void InsertEdge(Vertex V1, Vertex V2);    //插入边
+    void BuildEdge();    //将符合的点连上边
+    bool DFS(Vertex V); //DFS
+    void Init_Visited();  //将Visited全部变为false
+};
+
+int main() {
+    int Num_of_cro, jump_dis;   //鳄鱼数，跳跃距离
+    int x, y;   //坐标
+    
+    cin >> Num_of_cro >> jump_dis;
+    Graph G{ Num_of_cro + 1, jump_dis};   //创建图
+
+    //读入点的值
+    for (int i = 1; i <= Num_of_cro; i++) {
+        cin >> x >> y;
+        G.Data[i].x = x;
+        G.Data[i].y = y;
+    }
+
+    G.BuildEdge();   //将符合的点连上边
+    G.Init_Visited(); 
+
+    if (G.DFS(0)) cout << "Yes";
+    else cout << "No";
+
+    return 0;
+}
+
+Graph::Graph(int VertexNum, int jump_) {
+    Nv = VertexNum;
+    Ne = 0;
+    Data = new point[VertexNum];
+    Visited = new bool[VertexNum];
+
+    this->jump = jump_;
+    for (int i = 0; i < VertexNum; i++) {
+        for (int j = 0; j < VertexNum; j++) {
+            G[i][j] = 0;
+        }
+    }
+    Data[0].x = 0;
+    Data[0].y = 0;
+}
+
+void Graph::InsertEdge(Vertex V1, Vertex V2) {
+    //这里可以加判断有没有加过这条边
+
+    //插入边 <V1, V2>
+    G[V1][V2] = 1;
+    //插入边 <V2, V1>
+    G[V2][V1] = 1;
+
+    Ne++;//边数加1
+}
+
+void Graph::BuildEdge() {
+    for (int i = 1; i < Nv; i++) {
+        int dis = pow(Data[i].x, 2) + pow(Data[i].y, 2);
+        if ((pow(dis, 0.5) - 7.5) <= jump) {
+            InsertEdge(0, i);
+        }
+    }
+    for (int i = 1; i < Nv; i++) {
+        for (int j = i+1; j < Nv; j++) {
+            if ((pow(Data[i].x - Data[j].x, 2) + pow(Data[i].y - Data[j].y, 2)) <= pow(jump, 2)) {
+                InsertEdge(i, j);
+            }
+        }
+    }
+}
+
+bool Graph::DFS(Vertex V) {
+    int flag=0;
+    Visited[V] = true; // 标记V已访问,防止出现死循环 
+
+    for (Vertex i = 0; i < Nv; i++) {
+        if (!Visited[i] && G[V][i]) {
+            if ((pow(50 - abs(Data[i].x), 2) <= pow(jump, 2)) || (pow(50 - abs(Data[i].y), 2) <= pow(jump, 2))) {
+                flag=1;
+                break;
+            }
+            if (DFS(i)) {
+                flag = 1;
+                break;
+            }
+        }
+    }
+    if (flag) return true;
+    else return false;
+}
+
+void Graph::Init_Visited() {
+    for (int i = 0; i < Nv; i++) {
+        Visited[i] = false;
+    }
+}
--- Six_Degrees/main.cpp
+++ Six_Degrees/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <iomanip>
 #include <queue>
+#include <vector>
 
-#define MaxVertexNum 10    //最大顶点数设为100
+#define MaxVertexNum 100    //最大顶点数设为1000
 
 using namespace std;
-using Vertex=int;         // 用顶点下标表示顶点,为整型
+using Vertex = int;         // 用顶点下标表示顶点,为整型
 using WeightType = int;   //定义权重类型为int
 
 //图
@@ -17,14 +19,14 @@ public:
 public:
     Graph(int VertexNum);  //带参构造函数
     void InsertEdge(Vertex V1, Vertex V2);    //插入边
-    void DFS(Vertex V); //DFS
-    void BFS(Vertex V); //BFS
+    double BFS(Vertex V); //BFS
     void Init_Visited();  //将Visited全部变为false
 };
 
 int main() {
     int Num_of_Node, Num_of_Edge;  //顶点数、边数
     Vertex V1, V2;    //点
+    double percent;   //百分比
 
     cin >> Num_of_Node >> Num_of_Edge;  //输入顶点数、边数
     Graph G{ Num_of_Node };   //定义图
@@ -32,25 +34,15 @@ int main() {
     //输入图
     for (int i = 0; i < Num_of_Edge; i++) {
         cin >> V1 >> V2;   //输入边
-        G.InsertEdge(V1, V2);   //插入边
+        G.InsertEdge(V1-1, V2-1);   //插入边
     }
-    
-    //DFS
-    G.Init_Visited();  //将标识初始为false
+
+    //对每个结点输出与该结点距离不超过6的结点数占结点总数的百分比
     for (int i = 0; i < Num_of_Node; i++) {
-        if (G.Visited[i] != true) {
-            cout << "{ ";
-            G.DFS(i);
-            cout << "}\n";
-        }
-    }
-    G.Init_Visited();  //将标识初始为false
-    for (int i = 0; i < Num_of_Node; i++) {
-        if (G.Visited[i] != true) {
-            cout << "{ ";
-            G.BFS(i);
-            cout << "}\n";
-        }
+        G.Init_Visited();
+        percent = G.BFS(i);
+        cout.setf(ios::fixed);
+        cout <<i+1<< ": "<<fixed << setprecision(2) << percent <<"%"<< endl;
     }
 
     return 0;
@@ -64,7 +56,7 @@ Graph::Graph(int VertexNum) {
     for (int i = 0; i < VertexNum; i++) {
         for (int j = 0; j < VertexNum; j++) {
             G[i][j] = 0;
-       }
+        }
     }
 }
 
@@ -79,36 +71,41 @@ void Graph::InsertEdge(Vertex V1, Vertex V2) {
     Ne++;//边数加1
 }
 
-void Graph::DFS(Vertex V) {
-    cout << V << " ";    //输出V
-    Visited[V] = true; // 标记V已访问,防止出现死循环 
-
-    for (int i=0; i<Nv; i++) {
-        if (!Visited[i] && G[V][i]) {
-            DFS(i);
-        }
-    }
-}
-
-void Graph::BFS(Vertex V) {
+double Graph::BFS(Vertex V) {
     queue<Vertex> Q;
-    Vertex W;
 
-    cout << V << " ";    //输出V
+    Vertex W;
+    int distance = 0;  //用来记录距离
+    int Num_of_Node;  //用来计数每层有多少结点
+    int Num_of_all=0;  //用来计数与该结点距离不超过6的结点总数
+    int count=1;    //用来计数循环次数
+    double percent;  //百分比
+
     Visited[V] = true; // 标记V已访问,防止出现死循环 
     Q.push(V);
 
-    while (!Q.empty()) {
-        W = Q.front();
-        Q.pop();
-        for (int i = 0; i < Nv; i++) {
-            if (!Visited[i] && G[W][i]) {
-                cout << i << " ";    //输出V
-                Visited[i] = true; // 标记V已访问,防止出现死循环 
-                Q.push(i);
+    while (!Q.empty()&&distance<6) {
+        Num_of_Node = 0;     //每次都将该值设为0，计算每层有多少结点
+        for (int j = 0; j < count; j++) {   //这一层有多少，就循环几遍
+            W = Q.front();
+            Q.pop();
+   
+            for (int i = 0; i < Nv; i++) {
+                if (!Visited[i] && G[W][i]) {
+                    Visited[i] = true; // 标记V已访问,防止出现死循环 
+                    Q.push(i);
+                    Num_of_Node++;
+                }
             }
         }
+        count = Num_of_Node;
+        Num_of_all += Num_of_Node;
+        distance++;
     }
+
+    percent = static_cast<double>(Num_of_all+1) / static_cast<double>(Nv);//计算百分比，加上自己
+
+    return 100*percent;//返回值
 }
 
 void Graph::Init_Visited() {
